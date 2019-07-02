9eef5733853ba49c62f0273a9b80db363220bd1e
Aysegul Yonet <aysegulyonet@gmail.com>
2019-07-01 18:05:03 -0700

    feat(object-detection): step-1 quickstart

---
+++ sample.js
@@ -0,0 +1,22 @@
+const util = require('util');
+const TrainingApi = require("@azure/cognitiveservices-customvision-training");
+const PredictionApi = require("@azure/cognitiveservices-customvision-prediction");
+
+const setTimeoutPromise = util.promisify(setTimeout);
+
+const trainingKey = process.env.trainingKey;
+const predictionKey = process.env.predictionKey;
+const predictionResourceId = process.env.predictionResourceId;
+const sampleDataRoot = "./images";
+
+const endPoint = "https://westus2.api.cognitive.microsoft.com"
+
+const publishIterationName = "detectModel";
+
+const trainer = new TrainingApi.TrainingAPIClient(trainingKey, endPoint);
+
+(async () => {
+    console.log("Creating project...");
+    const domains = await trainer.getDomains()
+    const objDetectDomain = domains.find(domain => domain.type === "ObjectDetection");
+    const sampleProject = await trainer.createProject("Sample Obj Detection Project", { domainId: objDetectDomain.id });
